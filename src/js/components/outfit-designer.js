d640233b2c18b6a624a661d0da936e36b09023ec
Freddy Roberts <FreddyRoberts9@gmail.com>
2015-12-09 14:48:26 -0600

    added componets

---
+++ src/js/components/outfit-designer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class OutfitDesigner extends React.Component {
+  render () {
+    return (
+      <h1>OutfitDesigner</h1>
+    )
+  }
+}
+
+export default OutfitDesigner;
