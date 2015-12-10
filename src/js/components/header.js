8a53faa5cb8daf6bb4cb61cec5b20a92de8edfed
Kate Brock <katebrock@Kates-MacBook-Pro.local>
2015-12-10 15:19:46 -0600

    header nav

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -2,17 +2,33 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
 class Header extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {isVisible: false}
+    this.toggleNav = this.toggleNav.bind(this)
+  }
+  toggleNav(){
+    this.setState({isVisible: !this.state.isVisible})
+  }
   render () {
+    let className = 'headerNav';
+    if (this.state.isVisible) {
+      className = className + ' visible';
+    }
     return (
-      <header className="main">
-          <Link className="homelink" to="/"> Home </Link>
-          <Link className="loginlink" to="/login"> Login </Link>
-          <Link className="registerlink" to="/register"> Register </Link>
-          <Link className="dashboard" to="/dashboard"> Dashboard </Link>
-          <Link className="closet" to="/closet"> Closet </Link>
-          <Link className="oufits" to="/outfits"> Outfits </Link>
-          <Link className="outfit-designer" to="/outfitdesigner"> Outfit Designer </Link>
-          <Link className="publicfeed" to="/publicfeed"> Public Feed </Link>
+      <header className="mainheader">
+        <a onClick={this.toggleNav} href="#" className="click">Click</a>
+
+        <section className={className}>
+          <Link className="headerlink" to="/"> Home </Link>
+          <Link className="headerlink" to="/login"> Login </Link>
+          <Link className="headerlink" to="/register"> Register </Link>
+          <Link className="headerlink" to="/dashboard"> Dashboard </Link>
+          <Link className="headerlink" to="/closet"> Closet </Link>
+          <Link className="headerlink" to="/outfits"> Outfits </Link>
+          <Link className="headerlink" to="/outfitdesigner"> Outfit Designer </Link>
+          <Link className="headerlink" to="/publicfeed"> Public Feed </Link>
+        </section>
       </header>
     )
   }
