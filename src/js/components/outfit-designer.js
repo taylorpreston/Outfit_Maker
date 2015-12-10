cf6178313c1017affea394df2bcf26c4c78e4251
Freddy Roberts <FreddyRoberts9@gmail.com>
2015-12-10 14:56:56 -0600

    updated public feed, added media for mobile

--- src/js/components/outfit-designer.js
+++ src/js/components/outfit-designer.js
@@ -3,7 +3,9 @@ import React from 'react';
 class OutfitDesigner extends React.Component {
   render () {
     return (
+      <main className="outfitdesignerMain">
       <h1>OutfitDesigner</h1>
+      </main>
     )
   }
 }
