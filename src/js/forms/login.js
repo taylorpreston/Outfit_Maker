cd6bb0bfe0a51fc1d9cc35b21c57d8300aafbc3f
Kate Brock <katebrock@Kates-MacBook-Pro.local>
2015-12-11 11:27:47 -0600

    background image

--- src/js/forms/login.js
+++ src/js/forms/login.js
@@ -6,7 +6,7 @@ class Login extends React.Component {
   render () {
     console.log(this.props);
     return(
-      <section className="login user-form">
+      <section className="loginform">
         <input type="text" ref="username" placeholder="username"/>
         <input type="password" ref="password" placeholder="password"/>
         <input className="subBtn" type="submit" ref="submitBtn"/>
