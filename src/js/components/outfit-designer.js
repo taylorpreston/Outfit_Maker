f3901a15f53e6bf4d1edaab4093f81cdb869519c
Kate Brock <katebrock@Kates-MacBook-Pro.local>
2015-12-15 15:19:29 -0600

    html

--- src/js/components/outfit-designer.js
+++ src/js/components/outfit-designer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class OutfitDesigner extends React.Component {
+
   render () {
+    let designTops = this.props.designTops
     return (
       <main className="outfitdesignerMain">
       <h1>OutfitDesigner</h1>
+      <section className="designTops"></section>
+      <h2>tops</h2>
+      <section className="designBottom"></section>
+      <h3>bottoms</h3>
+      <section className="designShoes"></section>
+      <h4>shoes</h4>
+      <section className="designAccessories"></section>
+      <h5>accessories</h5>
       </main>
     )
   }
