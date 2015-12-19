b0852293c496214fcac8f8492460b408bbd6b451
Freddy Roberts <freddyroberts9@gmail.com>
2015-12-19 14:14:54 -0600

    updaed closet page, all items are now renderin on the pageg

--- src/js/components/article.js
+++ src/js/components/article.js
@@ -2,10 +2,14 @@ import React, { PropTypes } from 'react'
 
 class Article extends React.Component {
   render () {
+
+
+
     return(
-      <section className="outfitTypesList">
-        
-      </section>
+      <li>
+        <span>{this.props.discription}</span>
+        <img src={this.props.img}/>
+      </li>
     )
   }
 }
