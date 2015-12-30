b6fffc6a19f53c1e36ccd5342cc13431e4551d6f
Taylor Preston <taylor.preston.music@gmail.com>
2015-12-30 11:33:39 -0600

    outfit page working

--- src/js/components/outfit-item.js
+++ src/js/components/outfit-item.js
@@ -1,13 +1,22 @@
 import React, { PropTypes } from 'react'
 
 class OutfitItem extends React.Component {
-  render () {
-    
-    return{
-      <h2></h2>
-    }
 
+  render () {
+    console.log(this.props)
+    let outfit = this.props.outfit
+
+    return(
+      <div className="outfitDisplay">
+        <h2>{outfit.outfitName}</h2>
+        <img src={outfit.top.img}/>
+        <img src={outfit.bottomimg}/>
+        <img src={outfit.shoes.img}/>
+        <img src={outfit.accessories.img}/>
+      </div>
+    )
   }
+
 }
 
 export default OutfitItem;
