55f6e89139e14201133dedb2e4c51eb9ec58086b
Kate Brock <katebrock@Kates-MacBook-Pro.local>
2016-01-04 15:32:09 -0600

    style header

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('sass', function () {
 gulp.task('watch', function() {
   gulp.watch(['./src/**/*.js'], ['lint','babel']);
   gulp.watch(['./src/scss/*.scss'], ['sass']);
-  gulp.watch(['./src/index.html'], ['copy']);
+  gulp.watch(['./src/index.html', './src/img/**/*'], ['copy']);
 });
 
 gulp.task('copy', function () {
