b34dcf6d4c440bd594a0a3f7e43324a17d2c4572
Taylor Preston <taylor.preston.music@gmail.com>
2016-01-07 10:26:00 -0600

    fixed delete outfit bug

--- src/js/components/outfits.js
+++ src/js/components/outfits.js
@@ -10,14 +10,18 @@ class Outfits extends React.Component {
       userOutfits:[]
     }
     this.handleOutfitChange = this.handleOutfitChange.bind(this)
+    this.getOutfits = this.getOutfits.bind(this)
   }
 
 
   handleOutfitChange(){
+    console.log('getOutfitsRan');
+    this.getOutfits()
     this.forceUpdate()
   }
 
-  componentDidMount(){
+  getOutfits(){
+    console.log('getOutfitsRan');
     if(this.props.loggedIn === false){
       this.props.history.pushState(null, '/');
     }
@@ -43,6 +47,10 @@ class Outfits extends React.Component {
       }
     })
 
+  }
+
+  componentDidMount(){
+    this.getOutfits()
   }
 
   render () {
